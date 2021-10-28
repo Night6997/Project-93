f6b0053276a7f72f39cc56554a2f5dcd334b2e2b
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-10-28 11:11:43 +0530

    Add files via upload

--- LetsChat_room.js
+++ LetsChat_room.js
@@ -10,3 +10,41 @@ var firebaseConfig = {
 
   firebase.initializeApp(firebaseConfig);
 
+var user_name = localStorage.getItem("username");
+document.getElementById("username").innerHTML = "Welcome "+ user_name+"!";
+function addRoom(){
+
+      var room_name = document.getElementById("inputRoom").value;
+      firebase.database().ref("/").child(room_name).update({
+
+            roomkey: "purpose adding room"
+
+      });
+      
+      localStorage.setItem("inputRoom", room_name);
+
+      window.location = "LetsChat_page.html";
+
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {
+  document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+
+       Room_names = childKey;
+      console.log("Room name - "+ Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+      
+      });});}
+
+getData();
+
+function redirectToRoomName(name){
+
+     console.log(name);
+     localStorage.setItem("inputRoom", name);
+     window.location="LetsChat_page.html";
+
+}
+
+
