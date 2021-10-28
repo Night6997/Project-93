05ee7361775da2e7f550451b7c84cdc3b27b77ec
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-10-28 10:58:17 +0530

    Add files via upload

---
+++ LetsChat_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB-rlcfwI93zutxJlZbhPm_sp2LzIvwkdM",
+    authDomain: "letschatweb-40894.firebaseapp.com",
+    databaseURL: "https://letschatweb-40894-default-rtdb.firebaseio.com",
+    projectId: "letschatweb-40894",
+    storageBucket: "letschatweb-40894.appspot.com",
+    messagingSenderId: "384953594357",
+    appId: "1:384953594357:web:b2a02c697ae142915ff0bc"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
